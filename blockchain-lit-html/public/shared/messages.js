be4caf9c7fc6b49d261849e278cab6362e8b4fdd
Michael Luo <michael.luo0801@gmail.com>
2020-03-14 00:44:53 +1100

    add lit and websocket demo

---
+++ blockchain-lit-html/public/shared/messages.js
@@ -0,0 +1,9 @@
+// TODO: find out whether request / response prefixes can be generated with mapped types.
+export var MessageTypes;
+(function (MessageTypes) {
+    MessageTypes["GetLongestChainRequest"] = "GET_LONGEST_CHAIN_REQUEST";
+    MessageTypes["GetLongestChainResponse"] = "GET_LONGEST_CHAIN_RESPONSE";
+    MessageTypes["NewBlockRequest"] = "NEW_BLOCK_REQUEST";
+    MessageTypes["NewBlockAnnouncement"] = "NEW_BLOCK_ANNOUNCEMENT";
+})(MessageTypes || (MessageTypes = {}));
+//# sourceMappingURL=messages.js.map
